cfaca234dbd0c21babdc4e70ec81636fe55c0512
Jonathan Bonham <jwillb00@proton.me>
2025-01-26 18:05:07 -0700

    Finish ch. 2, begin layer struct

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include "fvector.h"
 
 int main(void) {
+    MATRIX temp;
     float input_arr[3][4] = {{1,2,3,2.5},
                              {2,5,-1,2},
                              {-1.5,2.7,3.3,-0.8}};
@@ -12,23 +13,41 @@ int main(void) {
     float weights_arr[3][4] = {{0.2,0.8,-0.5,1},
                                {0.5,-0.91,0.26,-0.5},
                                {-0.26,-0.27,0.17,0.87}};
-    float bias[] = {2,3,0.5};
 
     MATRIX weights = setMtx(3, 4, weights_arr);
-    MATRIX temp = transpose(weights, 3, 4);
+    temp = transpose(weights, 3, 4);
 
     freeMtx(weights, 3);
     weights = temp;
 
-    MATRIX out = multiply(input, 3, 4, weights, 4, 3);
+    float bias[] = {2,3,0.5};
+
+    float weights2_arr[3][4] = {{0.1,-0.14,0.5},
+                               {-0.5,0.12,-0.33},
+                               {-0.44,0.73,-0.13}};
+
+    MATRIX weights2 = setMtx(3, 4, weights2_arr);
+    temp = transpose(weights2, 3, 3);
+
+    freeMtx(weights2, 3);
+    weights2 = temp;
+
+    float bias2[] = {-1,2,-0.5};
+
+    MATRIX layer1_out = multiply(input, 3, 4, weights, 4, 3);
     
     freeMtx(input, 3);
     freeMtx(weights, 4);
 
-    vAdd(out, 3, 3, bias, 3);
+    vAdd(layer1_out, 3, 3, bias, 3);
+
+    MATRIX layer2_out = multiply(layer1_out, 3, 3, weights2, 3, 3);
+    vAdd(layer2_out, 3, 3, bias2, 3);
     
-    printMtx(out, 3, 3);
-    freeMtx(out, 3);
+    freeMtx(layer1_out, 3);
+    
+    printMtx(layer2_out, 3, 3);
+    freeMtx(layer2_out, 3);
 
     return 0;
 }
