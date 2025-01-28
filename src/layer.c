240ede61c36bdaf470ed4e4a4e1bffb18dc23b55
Jonathan Bonham <jwillb00@proton.me>
2025-01-27 19:44:14 -0700

    Begin function prototypes

--- src/layer.c
+++
@@ -1,4 +0,0 @@
-struct Layer_Dense {
-
-
-}
---
+++ src/layer.h
@@ -0,0 +1,9 @@
+
+
+typedef struct DenseLayer {
+    
+} DenseLayer;
+
+DenseLayer newDenseLayer(int n_inputs, int n_neurons);
+MATRIX forwardPass(DenseLayer layer);
+
